218eb049ef46b80f4769cb95c875d94bece635a3
arunabh-lejhro <arunabh.jain@lejhro.com>
2024-08-28 17:53:57 +0530

    eee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
 // Home.js or Home.tsx
 import Navbar from "../../components/navbar";
 import Footer from "../../components/footer";
-import Index from "../../pages/index";
+// import Index from "../../pages/index";
+import Hero from "../../components/hero";
+import AboutUs from "../../components/aboutus";
+import LearningMethods from "../../components/learningmethods";
+import Courses from "../../components/courses";
+import Feeds from "../../components/feeds";
 
 export default function Home() {
   return (
     <>
       <section>
-        <Navbar></Navbar>
-        <Index></Index>
-        <Footer></Footer>
+        <Navbar />
+        <Hero />
+        <AboutUs />
+        {/* <LearningMethods /> */}
+        <Courses />
+        <Feeds />
+        <Footer />
       </section>
     </>
   );
